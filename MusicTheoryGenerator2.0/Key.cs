14792358afaf9ede19f8723de39d9b2c3d026b45
TylerWuebker <70363323+TylerWuebker@users.noreply.github.com>
2022-08-15 10:14:50 -0500

    Initial Commit

--- MusicTheoryGenerator2.0/Key.cs
+++ MusicTheoryGenerator2.0/Key.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MusicTheoryGenerator2._0
+﻿namespace MusicTheoryGenerator2._0
 {
     public class Key
     {
